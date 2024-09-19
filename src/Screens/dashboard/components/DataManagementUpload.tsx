cbb91666520a546303de89fda09084a6792410cf
Esenwa Kachukwu Michael <michael.esenwa@gmail.com>
2024-09-19 23:36:22 +0100

    interact with the contract

--- src/Screens/dashboard/components/DataManagementUpload.tsx
+++ src/Screens/dashboard/components/DataManagementUpload.tsx
@@ -13,8 +13,10 @@ import { useStoreDataMutation } from "@/src/hooks/dataManagement/dataManagement"
 import { useStoreOnBlockchainMutation } from "@/src/hooks/blockchain/useBlockchain";
 import Spinner from "@/src/components/reuseables/Spinner";
 import Web3 from "web3";
-import DataStorage from "./DataStorage.json";
 import { useGetUserDataHashes } from "@/src/hooks/userHook/useUser";
+import { useWriteContract } from "wagmi";
+import { abi } from "@/src/contract/DataStorage2.json";
+
 
 declare global {
   interface Window {
@@ -23,26 +25,24 @@ declare global {
 }
 
 function DataManagementUpload() {
+  const {
+    writeContract,
+    writeContractAsync,
+    data: storedData,
+    isSuccess,
+    isPending: blocking,
+  } = useWriteContract();
+
   const { data: userHashes, refetch: getHash } = useGetUserDataHashes();
   const [isEnabled, setIsEnabled] = useState(false);
   const [web3, setWeb3] = useState<Web3 | null>(null);
   const [account, setAccount] = useState<string | null>(null);
   const {
     mutateAsync: storeOnBlockAsync,
-    isPending: blocking,
+    // isPending: blocking,
     // isError: isBlockError,
     // error: blockError,
   } = useStoreOnBlockchainMutation();
-
-  // Initialize Web3 in useEffect to ensure it only runs on the client side
-  // useEffect(() => {
-  //   if (window.ethereum) {
-  //     const web3Instance = new Web3(window.ethereum);
-  //     setWeb3(web3Instance);
-  //   } else {
-  //     console.error("MetaMask is not installed.");
-  //   }
-  // }, []);
 
   useEffect(() => {
     const initializeWeb3 = async () => {
@@ -54,13 +54,13 @@ function DataManagementUpload() {
           setWeb3(web3Instance);
 
           // Get the current account
-          const accounts = await web3Instance.eth.getAccounts();
-          setAccount(accounts[0]);
+          // const accounts = await web3Instance.eth.getAccounts();
+          // setAccount(accounts[0]);
 
           // Listen for account changes
-          window.ethereum.on("accountsChanged", (accounts: string[]) => {
-            setAccount(accounts[0]);
-          });
+          // window.ethereum.on("accountsChanged", (accounts: string[]) => {
+          //   setAccount(accounts[0]);
+          // });
         } catch (error) {
           console.error("Failed to initialize Web3", error);
         }
@@ -79,73 +79,6 @@ function DataManagementUpload() {
     };
   }, []);
 
-  // async function connectWallet() {
-  //   try {
-  //     if (typeof window.ethereum !== "undefined") {
-  //       const accounts = await window.ethereum.request({
-  //         method: "eth_requestAccounts",
-  //       });
-  //       return accounts[0];
-  //     }
-  //   } catch (error) {
-  //     console.error("User denied account access", error);
-  //     throw error;
-  //   }
-  // }
-
-  // const connectWallet2 = async () => {
-  //   if (!web3) {
-  //     console.error("Web3 is not initialized");
-  //     return;
-  //   }
-
-  //   try {
-  //     const accounts = await window.ethereum.request({
-  //       method: "eth_requestAccounts",
-  //     });
-  //     setAccount(accounts[0]);
-  //   } catch (error) {
-  //     console.error("Failed to connect wallet", error);
-  //   }
-  // };
-
-  // const signAndSubmitTransaction = async (
-  //   contractAddress: string,
-  //   contractAbi: any,
-  //   methodName: string,
-  //   args: any[]
-  // ): Promise<any> => {
-  //   if (!web3) throw new Error("Web3 is not initialized");
-  //   if (!account) throw new Error("No account connected");
-
-  //   try {
-  //     const contract = new web3.eth.Contract(contractAbi, contractAddress);
-  //     const data = contract.methods[methodName](...args).encodeABI();
-
-  //     const gasLimit = await web3.eth.estimateGas({
-  //       from: account,
-  //       to: contractAddress,
-  //       data: data,
-  //     });
-
-  //     const tx = {
-  //       from: account,
-  //       to: contractAddress,
-  //       data,
-  //       gas: gasLimit,
-  //     };
-
-  //     const req = await window.ethereum.request({
-  //       method: "eth_requestAccounts",
-  //     });
-  //     await storeOnBlockAsync({ signedTx: req[0] });
-
-  //     return req[0];
-  //   } catch (error) {
-  //     console.error("Error signing or submitting transaction:", error);
-  //     throw error;
-  //   }
-  // };
   const { refetch, data, isFetching, isError, error, isLoading } =
     useGetSecret(isEnabled);
   const { mutateAsync, isPending } = useStoreDataMutation();
@@ -161,24 +94,20 @@ function DataManagementUpload() {
           const formData = new FormData();
           formData.append("file", file);
           formData.append("secretKey", res.data.secretKey);
+
           // @ts-ignore
           mutateAsync(formData).then((res) => {
             if (res?.txHash && web3) {
-              getHash().then((res) => {
-                setUploadedFiles([]);
+              writeContractAsync({
+                abi,
+                address: "0x6b175474e89094c44da98b954eedeac495271d0f",
+                functionName: "storeData",
+                args: [res?.txHash],
+              }).then((res) => {
+                getHash().then((res) => {
+                  setUploadedFiles([]);
+                });
               });
-              // const contractAddress =
-              //   "0xbE1bC8C4e157EEAA5f41A6891E05A83a023c9Db1";
-              // const contractAbi = DataStorage.abi; // Your contract ABI
-              // const methodName = "storeData"; // Replace with the actual method name
-              // const args = [res.txHash]; // Arguments for contract method
-              // // User signs and submits the transaction
-              // signAndSubmitTransaction(
-              //   contractAddress,
-              //   contractAbi,
-              //   methodName,
-              //   args
-              // );
             }
           });
         }
@@ -188,6 +117,7 @@ function DataManagementUpload() {
     },
   });
 
+  console.log(storedData);
   return (
     <>
       <div
