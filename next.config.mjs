c05acb586a91f50fdfc977e32e607339b5f20e6d
Esenwa Kachukwu Michael <michael.esenwa@gmail.com>
2024-10-10 15:51:30 +0100

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  reactStrictMode: true,
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
+  // output: "export",
+  // reactStrictMode: true,
+  // trailingSlash: true,
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 export default nextConfig;
