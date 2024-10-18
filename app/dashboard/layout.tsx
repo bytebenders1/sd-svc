29197111a7800c20cdad10c7bb233ea8d370ca76
Esenwa Kachukwu Michael <michael.esenwa@gmail.com>
2024-10-18 23:25:50 +0100

    layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -13,11 +13,11 @@ function Layout({
     //   <div className="border-b h-px w-full border-neutral-300 lg:hidden" />
     //   <div className="px-2 md:px-8 lg:px-10 xl:px-28 ">{children}</div>
     // </>
-    <div className="p-2 grid grid-cols-12 w-screen h-screen bg-white">
-      <div className="col-span-3 max-w-[292px] border rounded-lg h-full border-gray-200 p-5">
+    <div className="p-6 flex gap-6 w-screen h-screen bg-white">
+      <div className="hidden xl:block xl:w-[20%] border rounded-lg h-full border-gray-200 p-5">
         <Sidebar />
       </div>
-      <div className="col-span-9 max-w-full h-full overflow-y-scroll -ml-5 pt-6">
+      <div className="w-full xl:w-9/12 h-full overflow-y-scroll  pt-6">
         {children}
       </div>
     </div>
