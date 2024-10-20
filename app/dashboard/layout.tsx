38b29a165ad312b8ebf8496daa51ddbb222e76db
Esenwa Kachukwu Michael <michael.esenwa@gmail.com>
2024-10-20 12:52:42 +0100

    changes

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,3 +1,4 @@
+import Nav from "@/src/components/reuseables/Nav";
 import Sidebar from "@/src/components/reuseables/Sidebar";
 import React from "react";
 
@@ -13,11 +14,14 @@ function Layout({
     //   <div className="border-b h-px w-full border-neutral-300 lg:hidden" />
     //   <div className="px-2 md:px-8 lg:px-10 xl:px-28 ">{children}</div>
     // </>
-    <div className="p-6 flex gap-6 w-screen h-screen bg-white">
+    <div className="p-4 xl:p-6 flex gap-6 w-screen h-screen bg-white">
       <div className="hidden xl:block xl:w-[20%] border rounded-lg h-full border-gray-200 p-5">
         <Sidebar />
       </div>
-      <div className="w-full xl:w-9/12 h-full overflow-y-scroll  pt-6">
+      <div className="w-full xl:w-9/12 h-full overflow-y-scroll  xl:pt-6">
+        <div className="xl:hidden mb-10">
+          <Nav />
+        </div>
         {children}
       </div>
     </div>
--- src/Screens/dashboard/overview/OverViewScreen.tsx
+++ src/Screens/dashboard/overview/OverViewScreen.tsx
@@ -22,15 +22,15 @@ function OverViewScreen() {
 
   return (
     <div>
-      <h1 className="text-2xl font-bold">Dashboard</h1>
-      <div className="mt-11">
+      <h1 className="text-lg md:text-2xl font-bold">Dashboard</h1>
+      <div className="mt-6 md:mt-11">
         <div className="grid grid-cols-2 gap-4">
-          <div className="border rounded-md p-5 col-span-1 flex items-end gap-3">
+          <div className="border rounded-md p-5 col-span-2 md:col-span-1 flex items-end gap-3">
             <div className="w-10/12">
-              <h5 className="text-neutral-400 text-sm font-inter">
+              <h5 className="text-neutral-400 text-sm md:text-sm font-inter">
                 Wallet address
               </h5>
-              <h3 className="text-3xl font-semibold font-inter">
+              <h3 className="md:text-xl xl:text-2xl font-semibold font-inter">
                 {address?.substring(0, 6)}...{address?.substring(38, 42)}
               </h3>
             </div>
@@ -41,11 +41,11 @@ function OverViewScreen() {
               Copy
             </Button>
           </div>
-          <div className="border rounded-md p-5 col-span-1 flex items-end gap-3 flex-col">
+          <div className="border rounded-md p-5 col-span-2 md:col-span-1 flex items-start justify-end gap-0 flex-col">
             <h5 className="text-neutral-400 !text-sm font-inter">
               KYC Verification Status
             </h5>
-            <div className="bg-green-100 rounded-full text-green-800 text-xs font-inter w-min px-3 py-1 mt-[18px]">
+            <div className="bg-green-100 rounded-full text-green-800 text-xs font-inter w-min px-3 py-1 mt-3 md:mt-[18px]">
               Verified
             </div>
           </div>
