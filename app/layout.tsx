b9dfcbcfa24a45ef58fec04f79e2af68e29b0c36
akinwunmi-akinrimisi <akinolaakinrimisi@gmail.com>
2024-12-11 21:28:44 +0100

    implemented google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Suspense } from "react";
 import Provider from "@/config/provider";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-HXXSNQJR38"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-HXXSNQJR38');
+            `,
+          }}
+        ></script>
+      </Head>
       <body className={inter.className}>
         <Provider>
           <Suspense>{children}</Suspense>
